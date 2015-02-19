021f8f694a049c4c84d076eaded8faaeeaa46b70
amzotti <azotti1337@gmail.com>
2015-02-18 21:33:29 -0800

    playFirst plays initial song in queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,6 +1,12 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
+  playFirst: function() {
+      var song = this.at(0);
+      song.play();
+      song.dequeue();
+  },
+
   initialize: function(){
   }
 
--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -7,11 +7,7 @@ var LibraryEntryView = Backbone.View.extend({
 
   events: {
     'click': function() {
-      if (document.getElementsByTagName("audio")[0].paused) {
-        this.model.play();
-      } else {
         this.model.enqueue();
-      }
     }
   },
 
